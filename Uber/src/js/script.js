8723548ef388b6ddab05dcb3790abb1add493d0d
Luffinage <Luffinage@gmail.com>
2023-05-14 18:54:02 +0200

    Added button click event

--- Uber/src/js/script.js
+++ Uber/src/js/script.js
@@ -1,21 +1,25 @@
+// window.onload = function() {
+//   var btns = document.querySelectorAll('.bnt');
+//   for(var i = 0; i < btns.length; i++) {
+//       var btn = btns[i];
+//       btn.onclick = function() {
+//           alert('ho ho ho');
+//       }
+//   }
+// }
 window.addEventListener("DOMContentLoaded", () => {
   const menu = document.querySelector(".menu"),
     menuItem = document.querySelectorAll(".menu_item"),
     hamburger = document.querySelector(".hamburger"),
-    // button = document.querySelector(".btn"),
+    buttons = document.querySelectorAll(".btn"),
     overlay = document.querySelector(".overlay"),
-    sorry = document.querySelector(".sorry");
-  // closeElem = document.querySelector(".close");
+    sorry = document.querySelector(".sorry"),
+    closeElem = document.querySelector(".close");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger_active");
     menu.classList.toggle("menu_active");
   });
-
-  // button.addEventListener("click", () => {
-  //   overlay.classList.add("active");
-  //   sorry.classList.add("active");
-  // });
 
   menuItem.forEach((item) => {
     item.addEventListener("click", () => {
@@ -23,14 +27,17 @@ window.addEventListener("DOMContentLoaded", () => {
       menu.classList.toggle("menu_active");
     });
   });
-  // closeElem.addEventListener("click", () => {
-  //   overlay.classList.remove("active");
-  //   sorry.classList.remove("active");
-  // });
-  $(".btn").on("click", function () {
-    $(".overlay,.sorry").fadeIn("slow");
-  });
-  $(".close").on("click", function () {
-    $(".overlay,#consultation,#order,#thanks,.sorry").fadeOut("slow");
+
+for(let i = 0; i < buttons.length; i++) {
+            let button = buttons[i];
+            button.onclick = function() {
+                overlay.classList.add("active");
+                sorry.classList.add("active");
+            }
+        };
+
+  closeElem.addEventListener("click", () => {
+    overlay.classList.remove("active");
+    sorry.classList.remove("active");
   });
 });
